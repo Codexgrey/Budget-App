1c5be1dd4fa6d83840567ffaa4fe3c9938f41976
Colingrey <Colingrey@users.noreply.github.com>
2020-08-22 23:08:10 +0100

    Adding a new item to our Budget Controller via exposed addItem method.

--- app.js
+++ app.js
@@ -27,6 +27,44 @@ var budgetController = (function() {
         }
     };
 
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID, typeArr;
+
+            typeArr = data.allItems[type]; //typeArr being inc or exp item arrays
+            
+            /* create/determine new ID
+             [1, 2, 3, 4, 5], next ID = 6; This is not plausible, as we expect to delete items from both arrays.
+             [1, 2, 4, 5, 8], next ID = 9; This is! As it allows for deleted items & is in order.
+             .'. ID = last ID + 1
+             */
+            if(typeArr.length > 0) {
+                ID = typeArr[typeArr.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            // create newItem based on 'inc' or 'exp' type
+            if(type === 'inc') {
+                newItem = new Income(ID, des, val);
+            } else if(type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            }
+
+            // add newItem into our data structure
+            typeArr.push(newItem);
+
+            // return newItem
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+
+    };
+
 })();
 
 
@@ -76,26 +114,29 @@ var UIController = (function() {
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    var ctrlAddItem, input, DOM, setupEventListeners;
+    var ctrlAddItem, setupEventListeners;
 
     setupEventListeners = function() {
-        DOM = UICtrl.getDOMstr();
+        var DOM;
 
+        DOM = UICtrl.getDOMstr();
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
         document.addEventListener('keypress', function(event) {
-        if(event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem(); 
-        }
-    });
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem(); 
+            }
+        });
 
-    }
+    };
 
     ctrlAddItem = function() {
+        var input, newItem;
 
         // get the field input data
         input = UICtrl.getInput();
 
         // add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // add new item to the UI
 
